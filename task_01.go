be7160ced2f6dfc1cddf2f84a94d717388e2f54d
Aziz <aziz.azamhodjaev96@mail.ru>
2022-09-06 16:46:13 +0500

    first commit

---
+++ task_01.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int = 3, 4
+
+	// fmt.Scanf("%d", &a)
+	// fmt.Scanf("%d", &b)
+	fmt.Printf("a = %d, b = %d\n", a, b)
+	var c int
+	c = a
+	a = b
+	b = c
+	fmt.Printf("a = %d, b = %d\n", a, b)
+}
